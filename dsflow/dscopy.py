2c99045163b4ffb3019e3ce0d247d28bb60d8a3a
tomoemon <bach48+github_tomoemon@gmail.com>
2019-09-25 16:17:18 +0900

    revert

--- dsflow/dscopy.py
+++ dsflow/dscopy.py
@@ -1,4 +1,1 @@
-# -*- coding: utf-8 -*-
-if __name__ == '__main__':
-    from .dsflow import dscopy
-    dscopy.run()
+dsflow/dscopy.py
--- dsflow/dsdelete.py
+++ dsflow/dsdelete.py
@@ -1,4 +1,1 @@
-# -*- coding: utf-8 -*-
-if __name__ == '__main__':
-    from .dsflow import dsdelete
-    dsdelete.run()
+dsflow/dsdelete.py
